e8df619f1d62d58fd84b1d97e31bf40d2e732431
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-03-02 17:56:26 +0100

    The algorithm almost works. Not handling the case with 6 ip's properly.

--- task_two.py
+++ task_two.py
@@ -8,5 +8,25 @@ class Ip:
 
 
 ip_range = [Ip("10.0.0.1", False), Ip("10.0.0.2", False), Ip("10.0.0.3", False), Ip("10.0.0.4", False),
-           Ip("10.0.0.5", False), Ip("10.0.0.6", False)]
+            Ip("10.0.0.5", False), Ip("10.0.0.6", False)]
 
+flag = True
+
+i = 0
+counter = 0
+while flag:
+    ting = True
+
+    i = (i + 1) % len(ip_range)
+    if not ip_range[i].checked:
+        ip_range[i].checked = True
+        counter += 1
+
+        if counter == len(ip_range):
+            print(ip_range[i].ip)
+            flag = False
+    else:
+        ting = False
+
+    if ting:
+        i = (i + 1) % len(ip_range)
