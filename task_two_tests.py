f26aa9be722538fd932f14d395308b2ea29e9033
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-03-02 21:00:38 +0100

    Changed name to follow the naming standard for the unittest package.

--- task_two_tests.py
+++ task_two_tests.py
@@ -4,25 +4,25 @@ from task_two import find_last_scanned_ip, Ip
 
 
 class TestTaskTwo(unittest.TestCase):
-    def find_last_scanned_ip_test_6_ips(self):
+    def test_find_last_scanned_ip_6_ips(self):
         ip_range = [Ip("10.0.0.1", False), Ip("10.0.0.2", False), Ip("10.0.0.3", False), Ip("10.0.0.4", False),
                     Ip("10.0.0.5", False), Ip("10.0.0.6", False)]
         self.assertEqual("10.0.0.5", find_last_scanned_ip(ip_range))
 
-    def find_last_scanned_ip_test_5_ips(self):
+    def test_find_last_scanned_ip_5_ips(self):
         ip_range = [Ip("10.0.0.1", False), Ip("10.0.0.2", False), Ip("10.0.0.3", False), Ip("10.0.0.4", False),
                     Ip("10.0.0.5", False)]
         self.assertEqual("10.0.0.3", find_last_scanned_ip(ip_range))
 
-    def find_last_scanned_ip_test_4_ips(self):
+    def test_find_last_scanned_ip_4_ips(self):
         ip_range = [Ip("10.0.0.1", False), Ip("10.0.0.2", False), Ip("10.0.0.3", False), Ip("10.0.0.4", False)]
         self.assertEqual("10.0.0.1", find_last_scanned_ip(ip_range))
 
-    def find_last_scanned_ip_test_3_ips(self):
+    def test_find_last_scanned_ip_3_ips(self):
         ip_range = [Ip("10.0.0.1", False), Ip("10.0.0.2", False), Ip("10.0.0.3", False)]
         self.assertEqual("10.0.0.3", find_last_scanned_ip(ip_range))
 
-    def find_last_scanned_ip_test_2_ips(self):
+    def test_find_last_scanned_ip_2_ips(self):
         ip_range = [Ip("10.0.0.1", False), Ip("10.0.0.2", False)]
         self.assertEqual("10.0.0.1", find_last_scanned_ip(ip_range))
 
