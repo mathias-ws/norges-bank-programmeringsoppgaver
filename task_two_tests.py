47f1c09537388d557598e1e1408dd430fabf18f0
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-03-02 20:54:11 +0100

    Updated function names to better reflect what they do.

--- task_two_tests.py
+++ task_two_tests.py
@@ -4,25 +4,25 @@ from task_two import find_last_scanned_ip, Ip
 
 
 class TestTaskTwo(unittest.TestCase):
-    def test_6_ips(self):
+    def find_last_scanned_ip_test_6_ips(self):
         ip_range = [Ip("10.0.0.1", False), Ip("10.0.0.2", False), Ip("10.0.0.3", False), Ip("10.0.0.4", False),
                     Ip("10.0.0.5", False), Ip("10.0.0.6", False)]
         self.assertEqual("10.0.0.5", find_last_scanned_ip(ip_range))
 
-    def test_5_ips(self):
+    def find_last_scanned_ip_test_5_ips(self):
         ip_range = [Ip("10.0.0.1", False), Ip("10.0.0.2", False), Ip("10.0.0.3", False), Ip("10.0.0.4", False),
                     Ip("10.0.0.5", False)]
         self.assertEqual("10.0.0.3", find_last_scanned_ip(ip_range))
 
-    def test_4_ips(self):
+    def find_last_scanned_ip_test_4_ips(self):
         ip_range = [Ip("10.0.0.1", False), Ip("10.0.0.2", False), Ip("10.0.0.3", False), Ip("10.0.0.4", False)]
         self.assertEqual("10.0.0.1", find_last_scanned_ip(ip_range))
 
-    def test_3_ips(self):
+    def find_last_scanned_ip_test_3_ips(self):
         ip_range = [Ip("10.0.0.1", False), Ip("10.0.0.2", False), Ip("10.0.0.3", False)]
         self.assertEqual("10.0.0.3", find_last_scanned_ip(ip_range))
 
-    def test_2_ips(self):
+    def find_last_scanned_ip_test_2_ips(self):
         ip_range = [Ip("10.0.0.1", False), Ip("10.0.0.2", False)]
         self.assertEqual("10.0.0.1", find_last_scanned_ip(ip_range))
 
