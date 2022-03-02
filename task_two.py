315b8ac3de4b1c6f11b276e57ed487e7da83968a
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-03-02 20:28:06 +0100

    Created a method that can generate an ip range.

--- task_two.py
+++ task_two.py
@@ -1,14 +1,38 @@
 from dataclasses import dataclass
 
 
+# Class used to define an ip object.
 @dataclass
 class Ip:
     ip: str
     checked: bool
 
 
-ip_range = [Ip("10.0.0.1", False), Ip("10.0.0.2", False), Ip("10.0.0.3", False), Ip("10.0.0.4", False),
-            Ip("10.0.0.5", False)]
+# Function inspired by: https://tkit.dev/2011/09/11/how-to-generate-an-ip-range-list-in-python/
+# Function that takes in a start and an end ip and generates a list of the ips in the range.
+def generate_ip_range(start_ip, end_ip):
+    ip_range = []
+
+    # Casting to ints is inspired by: https://stackoverflow.com/a/5306094
+    first_ip = [int(ip) for ip in (start_ip.split("."))]
+    last_ip = [int(ip) for ip in (end_ip.split("."))]
+
+    temp_ip = first_ip
+
+    ip_range.append(Ip(start_ip, False))
+
+    while temp_ip != last_ip:
+        first_ip[3] += 1
+
+        # Counts up every octet.
+        for i in (3, 2, 1):
+            if temp_ip[i] == 256:
+                temp_ip[i] = 0
+                temp_ip[i - 1] += 1
+
+        ip_range.append(Ip(".".join(map(str, temp_ip)), False))
+
+    return ip_range
 
 
 def find_last_scanned_ip(ip_list):
