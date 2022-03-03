9cc57777b79ede9dc5e439851374a7e4aa66bbb0
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-03-03 11:12:54 +0100

    Added running the task 1.

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+from task_one import compare_ips_in_files
 from task_two import find_last_scanned_ip, generate_ip_range
 
-print("Creating the ip range.")
+print("####################Task 1####################")
+print(compare_ips_in_files(
+    input("Please enter the folder where the csv file(s) and the txt file is located in: ")))
+
+print("\n\n####################Task 2####################")
+print("Creating the ip range 10.0.0.1-10.255.255.254.")
 ip_range = generate_ip_range("10.0.0.1", "10.255.255.254")
 
 print("Ip range created, starting to find the last ip to scan.")
