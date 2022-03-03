541385ca40d8f94b2ebc22b97c0078811c52ed66
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2022-03-03 10:49:16 +0100

    Added the functionality to compare the files in a specified folder.

--- task_one.py
+++ task_one.py
@@ -19,8 +19,40 @@ def read_file(path):
 
 
 def get_all_files_in_folder(path):
+    """Gets a list of files in a given folder."""
     return os.listdir(path)
 
 
-print(get_all_files_in_folder(r'task_one_example_data_set'))
-print(read_file(r'task_one_example_data_set/ips2.csv'))
+def compare_ips_in_lists(list_to_compare, list_to_be_compared):
+    """Compares two lists and returns the index of where a match occurs."""
+    for ip_to_compare in list_to_compare:
+        for ip_to_be_compared in list_to_be_compared:
+            if ip_to_compare == ip_to_be_compared:
+                return list_to_be_compared.index(ip_to_be_compared)
+    return None
+
+
+def find_txt_file(files):
+    """Finds a txt file in a list of files."""
+    for file in files:
+        if file.endswith('.txt'):
+            return file
+    return None
+
+
+def compare_ips_in_files(path):
+    """Takes in a folder path containing csv file(s) and a txt file. It does then compare every csv file with
+    the txt file. It then returns a string with the file name and line of the first match."""
+    files = get_all_files_in_folder(path)
+
+    txt_file = find_txt_file(files)
+
+    files.remove(txt_file)
+    txt_file = read_file(path + '/' + txt_file)
+
+    for csv_file in files:
+        line = compare_ips_in_lists(txt_file, read_file(path + '/' + csv_file))
+
+        if line is not None:
+            return "Found match in csv file: " + csv_file + " on line: " + str(line + 1)
+    return "No matches found."
